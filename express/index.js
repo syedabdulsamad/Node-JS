b42df4325448b037073007ef9e311683b4b409e2
Abdul Samad <abdulsamad@TetraNoodle.local>
2019-09-12 23:03:27 +0300

    Added post request to add a course in static array.

--- express/index.js
+++ express/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
 const port = process.env.PORT || 3000
 
 
 const courses = [
     
-    {id:"1", name: "course1"},
-    {id:"2", name: "course2"},
-    {id:"3", name: "course3"},
-    {id:"4", name: "course4"},
-    {id:"5", name: "course5"},
-    {id:"6", name: "course6"}
+    {id:1   , name: "course1"},
+    {id:2   , name: "course2"},
+    {id:3   , name: "course3"},
+    {id:4   , name: "course4"},
+    {id:5   , name: "course5"},
+    {id:6   , name: "course6"}
 ];
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -29,7 +29,7 @@ app.get("/api/courses/", (request, response) => {
 
 app.get('/api/courses/:id', (request, response) => {
     
-    const course = courses.find(c => c.id === request.params.id);
+    const course = courses.find(c => c.id === parseInt(request.params.id));
     if(!course) {
         response.status(404).send(`The course with id: ${request.params.id} is not found.`);
     } else {
@@ -38,6 +38,20 @@ app.get('/api/courses/:id', (request, response) => {
 
 });
 
+app.post("/api/courses", (request, response) => {
+    const newCourse = {
+        id : getCourses().length + 1,
+        name : request.body.name
+    };
+    addCourse(newCourse);
+    response.send(newCourse);
+
+});
+
+
+function addCourse(course) {
+    courses.push(course);
+}
 
 function getCourses() {
     return courses;
